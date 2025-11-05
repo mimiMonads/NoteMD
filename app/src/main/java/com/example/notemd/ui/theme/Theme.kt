1b75d7ea5586c013852cc1f2d9bb7de60728dde2
mimiMonads <mimi@monads.dev>
2025-11-05 11:51:37 +0000

    More theme stuff

--- app/src/main/java/com/example/notemd/ui/theme/Theme.kt
+++ app/src/main/java/com/example/notemd/ui/theme/Theme.kt
@@ -9,50 +9,71 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+private val LightColors = lightColorScheme(
+    primary = PrimaryBlue,
+    onPrimary = ColorWhite,
+    primaryContainer = PrimaryBlue.copy(alpha = 0.2f),
+    onPrimaryContainer = PrimaryBlueDark,
+    secondary = SecondaryPurple,
+    onSecondary = ColorWhite,
+    secondaryContainer = SecondaryPurple.copy(alpha = 0.2f),
+    onSecondaryContainer = SecondaryPurple,
+    background = NeutralBackground,
+    onBackground = NeutralOnBackground,
+    surface = ColorWhite,
+    onSurface = NeutralOnBackground
 )
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+private val DarkColors = darkColorScheme(
+    primary = PrimaryBlue,
+    onPrimary = ColorWhite,
+    primaryContainer = PrimaryBlueDark,
+    onPrimaryContainer = ColorWhite,
+    secondary = SecondaryPurple,
+    onSecondary = ColorWhite,
+    secondaryContainer = PrimaryBlueDark,
+    onSecondaryContainer = ColorWhite,
+    background = NeutralOnBackground,
+    onBackground = NeutralBackground,
+    surface = NeutralOnBackground,
+    onSurface = NeutralBackground
 )
 
 @Composable
 fun NoteMDTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
+    useDarkTheme: Boolean = isSystemInDarkTheme(),
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+            if (useDarkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
 
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+        useDarkTheme -> DarkColors
+        else -> LightColors
+    }
+
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.primary.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !useDarkTheme
+        }
     }
 
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
+        shapes = Shapes,
         content = content
     )
 }
